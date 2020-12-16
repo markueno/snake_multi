f9aa15270e70c6fa1ea123c9b28bb297f2cd7b0c
markueno <45034691+markueno@users.noreply.github.com>
2020-12-16 10:07:59 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,113 @@
+const io = require('socket.io')();
+const express = require('express')
+const app = express()
+const { initGame, gameLoop, getUpdatedVelocity } = require('./game');
+const { FRAME_RATE } = require('./constants');
+const { makeid } = require('./utils');
+
+const state = {};
+const clientRooms = {};
+
+
+app.get('/',(req, res)=>{
+  res.sendFiler(__dirname +'/index.html')
+})
+
+io.on('connection', client => {
+// wtf is key down
+  client.on('keydown', handleKeydown);
+  client.on('newGame', handleNewGame);
+  client.on('joinGame', handleJoinGame);
+
+  function handleJoinGame(roomName) {
+    // making rooms
+    const room = io.sockets.adapter.rooms[roomName];
+
+    let allUsers;
+    if (room) {
+      // so the 2 sockets in the current room
+      allUsers = room.sockets;
+    }
+
+    let numClients = 0;
+    if (allUsers) {
+      numClients = Object.keys(allUsers).length;
+    }
+
+    if (numClients === 0) {
+      // so no one at home?
+      client.emit('unknownCode');
+      return;
+    } else if (numClients > 1) {
+      // there are more than 2?
+      client.emit('tooManyPlayers');
+      return;
+    }
+
+    clientRooms[client.id] = roomName;
+
+    client.join(roomName);
+    client.number = 2;
+    client.emit('init', 2);
+
+    startGameInterval(roomName);
+  }
+
+  function handleNewGame() {
+    let roomName = makeid(5);
+    clientRooms[client.id] = roomName;
+    client.emit('gameCode', roomName);
+
+    state[roomName] = initGame();
+
+    client.join(roomName);
+    client.number = 1;
+    client.emit('init', 1);
+  }
+
+  function handleKeydown(keyCode) {
+    const roomName = clientRooms[client.id];
+    if (!roomName) {
+      return;
+    }
+    try {
+      keyCode = parseInt(keyCode);
+    } catch(e) {
+      console.error(e);
+      return;
+    }
+
+    const vel = getUpdatedVelocity(keyCode);
+
+    if (vel) {
+      state[roomName].players[client.number - 1].vel = vel;
+    }
+  }
+});
+
+function startGameInterval(roomName) {
+  const intervalId = setInterval(() => {
+    const winner = gameLoop(state[roomName]);
+
+    if (!winner) {
+      emitGameState(roomName, state[roomName])
+    } else {
+      emitGameOver(roomName, winner);
+      state[roomName] = null;
+      clearInterval(intervalId);
+    }
+  }, 1000 / FRAME_RATE);
+}
+
+function emitGameState(room, gameState) {
+  // Send this event to everyone in the room.
+  io.sockets.in(room)
+    .emit('gameState', JSON.stringify(gameState));
+}
+
+function emitGameOver(room, winner) {
+  io.sockets.in(room)
+    .emit('gameOver', JSON.stringify({ winner }));
+}
+
+io.listen(process.env.PORT || 3000);
